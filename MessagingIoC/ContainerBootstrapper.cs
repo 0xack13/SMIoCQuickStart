c0580bec5fe38308c5cae2e54bc4a8ef07f395fd
0xack13 <0xack13@gmail.com>
2014-12-09 12:51:59 +0300

    New Terse

--- MessagingIoC/ContainerBootstrapper.cs
+++ MessagingIoC/ContainerBootstrapper.cs
@@ -14,6 +14,7 @@ namespace MessagingIoC
             // Initialize the static ObjectFactory container 
             ObjectFactory.Initialize(x =>
             {
+                // New terse, “jQuery-esque” syntax for the Registry DSL.  Let’s just write off “ForRequestedType<IFoo>().TheDefault.Is.OfConcreteType<Foo>()” as a well meant experiment, shall we?  It’s still in the codebase, but it’s marked as [Obsolete].  Nobody liked it and I thought it was annoying in real life usage.  Now, just do:  For<IFoo>().Use<Foo>();  My whole goal was to reduce friction. 
                 x.For<IDisplayMessage>().Use<DisplayMessage>();
             });
         }
