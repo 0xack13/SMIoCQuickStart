270f403c31d48b8f29c3538c0ab105e584eaccc9
0xack13 <0xack13@gmail.com>
2014-12-09 12:51:04 +0300

    Call anotherdisplaymessage

---
+++ MessagingIoC/ContainerBootstrapper.cs
@@ -0,0 +1,21 @@
+﻿using StructureMap;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessagingIoC
+{
+    public static class ContainerBootstrapper
+    {
+        public static void BootstrapStructureMap()
+        {
+            // Initialize the static ObjectFactory container 
+            ObjectFactory.Initialize(x =>
+            {
+                x.For<IDisplayMessage>().Use<DisplayMessage>();
+            });
+        }
+    }
+}
--- MessagingIoC/Program.cs
+++ MessagingIoC/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StructureMap;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,16 @@ namespace MessagingIoC
     {
         static void Main(string[] args)
         {
+            IDisplayMessage idm = new DisplayMessage();//creating instance of class A
+            ContainerBootstrapper.BootstrapStructureMap();
+            var adm = StructureMap.ObjectFactory.GetInstance<AnotherDisplayMessage>();
+
+            //assign the instance to the classB property of class A 
+            idm = adm;
+
+            //Call the method
+            Console.WriteLine(adm.message());
+            Console.Read();
         }
     }
 }
