4cfb73f3b4479d3131bd2db45751cc0f3182bc9b
0xack13 <0xack13@gmail.com>
2014-12-22 13:08:12 +0300

    Action Speed Filter inital implementation

--- ASPActionFilterApp/Filters/ActionSpeedProfilerAttribute.cs
+++ ASPActionFilterApp/Filters/ActionSpeedProfilerAttribute.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace ASPActionFilterApp.Filters
 {
-    public class ActionSpeedProfilerAttribute
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)] 
+    public class ActionSpeedProfilerAttribute : FilterAttribute, IActionFilter
     {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            throw new NotImplementedException();
+        }
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            throw new NotImplementedException();
+        } 
     }
 }
